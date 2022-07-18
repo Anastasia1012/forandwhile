fe798c6c5ebdfecfb20dbc239a6770d7075077d2
Anastasia <79032456178@yandex.ru>
2022-07-18 19:51:29 +0300

    training with for and while

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+            // Напишите программу, которая покажет количество чётных чисел в массиве.
+            Console.WriteLine("Задайте массив заполненный случайными положительными трёхзначными числами. " +
+                "Напишите программу, которая покажет количество чётных чисел в массиве");
+            int[] array;
+            array = new int[5];
+            int i = 0;
+            int even = 0;
+            Random random = new Random();
+            while(i < array.Length)
+            {
+                array[i] = random.Next(100, 999);
+                Console.WriteLine(array[i]);
+                i++;
+
+            }
+
+            for (i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    even = even + 1;
+
+            }
+            
+            //for (i = 0; )
+            Console.WriteLine("количество четных чисел " + even);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
